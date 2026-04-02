19d8ca484302b4f8a571057348c6de54c21e680b
Pritam <debnathpritam0802@gmail.com>
2026-04-02 19:14:24 +0530

    retain theme  on refresh

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,16 @@
 import * as React from 'react'
 import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { useThemeStore } from "@/store/theme.store"
+const { theme } = useThemeStore.getState()
+
+const setAppTheme = () => {
+  if (theme == "dark") {
+    document.body.classList.add("dark")
+  } else {
+    document.body.classList.remove("dark")
+  }
+}
+setAppTheme()
 
 export const Route = createRootRoute({
   component: RootComponent,
--- src/store/theme.store.ts
+++ src/store/theme.store.ts
@@ -3,12 +3,13 @@ import { createStore, create, } from "zustand"
 import { persist } from "zustand/middleware"
 
 export const themes = ["dark", "light"] as const
+const root = document
 
-type Theme =  typeof themes[number]
+type Theme = typeof themes[number]
 
 type Store = {
     theme: Theme;
-    setTheme: (theme: Theme)=>void;
+    setTheme: (theme: Theme) => void;
 }
 
 const store = create<Store>()
@@ -17,7 +18,14 @@ export const useThemeStore = store(
     persist(
         (set) => ({
             theme: "light",
-            setTheme:(theme: Theme)=>set({theme: theme})
+            setTheme: (theme: Theme) => {
+                set({ theme: theme })
+                if (theme == "dark") {
+                    root.body.classList.add("dark")
+                } else {
+                    root.body.classList.remove("dark")
+                }
+            }
         }),
         {
             name: "theme"
