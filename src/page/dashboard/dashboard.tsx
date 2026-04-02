aba08f51b039967f7732f73cb783c29c7b4996ae
Pritam <debnathpritam0802@gmail.com>
2026-04-02 13:19:48 +0530

    added summary cards for Total Balance, Income and Expenses

--- src/page/dashboard/dashboard.tsx
+++ src/page/dashboard/dashboard.tsx
@@ -1,12 +1,22 @@
+import SummaryCard from "@/feature/flow/component/summary-card";
 import React, { useEffect, useState } from "react";
 
 
 export default function Dashboard() {
 
+  const income = 900
+  const expense = 400
+  const balance = income - expense;
+
   return (
     <div className="h-screen  flex flex-col">
-      <h1 className="p-2 bg-gray-200">FutureBlink Assessment (MERN app)</h1>
-    
+      <h1 className="p-2 bg-gray-200">Finance Dashboard</h1>
+      <div className="flex items-center p-4 gap-4">
+   <SummaryCard title="Total Balance" value={balance} />
+      <SummaryCard title="Income" value={income} />
+      <SummaryCard title="Expenses" value={expense} />
+      </div>
+   
     </div>
   );
 }
